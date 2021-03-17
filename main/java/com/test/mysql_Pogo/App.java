761dd69f2b83d8e5889efe4d9f1edb014bb38ea9
ankita13-1997 <parhiankita@gmail.com>
2021-03-17 21:14:30 +0530

    welcome to payroll of employee

---
+++ main/java/com/test/mysql_Pogo/App.java
@@ -0,0 +1,13 @@
+package com.test.mysql_Pogo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
